929ac719b2583bce5d0d247910fc112d63e3087e
arsabbir674 <rahmansabbir29291@gmail.com>
2026-04-30 21:40:04 +0600

    Add files via upload

---
+++ c-4_counting_oreety_numbers.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int l, r;
+        cin >> l >> r;
+        int cnt = 0;
+        for (int i = l; i <= r; i++) {
+            if (i % 10 == 2 || i % 10 == 3 || i % 10 == 9) {
+                cnt++;
+            }
+        }
+        cout << cnt << endl;
+    }
+}
