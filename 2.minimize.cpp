f05d66e296633cbf656d4b6716215d768ad57bea
arsabbir674 <rahmansabbir29291@gmail.com>
2026-04-30 21:12:19 +0600

    Add files via upload

---
+++ 2.minimize.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int a,b;
+        cin >> a >> b;
+         int result = 10000;
+        for(int c = a ; c<=b ;c++)
+        {
+            int new_result = (c-a)+(b-c);
+            result = min(result,new_result);
+
+
+
+
+        }
+        cout << result << endl;
+    }
+}
